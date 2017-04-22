cd633f64894b20abf7110a461e617421fa3a35a3
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2017-04-22 11:18:25 +0200

    refacto(README)

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,24 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SampleComponent } from './sample.component';
-import { SampleDirective } from './sample.directive';
-import { SamplePipe } from './sample.pipe';
-import { SampleService } from './sample.service';
+import { KonamiDirective } from './konami.directive';
 
-export * from './sample.component';
-export * from './sample.directive';
-export * from './sample.pipe';
-export * from './sample.service';
+export * from './konami.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
+    KonamiDirective
   ],
   exports: [
-    SampleComponent,
-    SampleDirective,
-    SamplePipe
+    KonamiDirective
   ]
 })
-export class SampleModule {
+export class KonamiModule {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: SampleModule,
-      providers: [SampleService]
+      ngModule: KonamiModule
     };
   }
 }
---
+++ src/konami.directive.ts
@@ -0,0 +1,37 @@
+import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
+
+@Directive({
+  selector: '[konami]'
+})
+export class KonamiDirective {
+
+  @Output()
+  private konami: EventEmitter<void>;
+
+  private sequence: string[];
+
+  private konamiCode: string[];
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.sequence.push(event.keyCode.toString());
+
+    if (this.sequence.length > this.konamiCode.length) {
+      this.sequence.shift();
+    }
+
+    if (this.isKonamiCode()) {
+      this.konami.emit();
+    }
+  }
+
+  constructor() {
+    this.konami = new EventEmitter<void>();
+    this.sequence = [];
+    this.konamiCode = ['38', '38', '40', '40', '37', '39', '37', '39', '66', '65'];
+  }
+
+  isKonamiCode(): boolean {
+    return this.konamiCode.every((code: string, index: number) => code === this.sequence[index]);
+  }
+}
